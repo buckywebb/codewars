76ff9a36a99b8fb3db0a282895356875efc2c4e4
buckywebb <bucky_webb@yahoo.com>
2023-03-08 22:25:05 -0500

    3/8/23

--- solutions.js
+++ solutions.js
@@ -6,6 +6,7 @@ function getRealFloor(n) {
   }
 
 //===========================
+
 function findMultiples(integer, limit) {
   let arr = []
   for (let i = 1; integer * i <= limit; i++){
@@ -13,3 +14,17 @@ function findMultiples(integer, limit) {
   }
   return arr;
 }
+
+//===========================
+
+function peopleWithAgeDrink(old) {
+  if (old < 14){
+    return 'drink toddy'
+  }else if(old < 18){
+    return 'drink coke'
+  }else if(old < 21){
+    return 'drink beer'
+  }else if(old >= 21){
+    return 'drink whisky'
+  }
+};
