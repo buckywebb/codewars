0cf768ff43c60ca78b92f412484c820bb26b163a
buckywebb <bucky_webb@yahoo.com>
2023-03-21 23:56:12 -0400

    3/21/23

--- solutions.js
+++ solutions.js
@@ -102,3 +102,13 @@ function warnTheSheep(queue) {
 //=================================
 
 const getChar = c => String.fromCharCode(c)
+
+//=================================
+
+const mouthSize = animal => animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+
+//=================================
+
+const _if = (bool, func1, func2) => bool ? func1() : func2()
+
+//=================================
