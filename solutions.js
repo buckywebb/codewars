f820b82530218ba73397a65d6f03aace133795c6
buckywebb <bucky_webb@yahoo.com>
2023-03-23 22:29:26 -0400

    3/23/23

--- solutions.js
+++ solutions.js
@@ -134,3 +134,16 @@ function hello(name){
     return "Hello, World!";
   }
 }
+
+//==================================
+
+function pipeFix(numbers){
+  let first = numbers[0]
+  let last = numbers[numbers.length -1]
+  let arr = []
+  for(let i = first; i <= last; i++){
+    arr.push(i)
+  }
+  return arr
+}
+
