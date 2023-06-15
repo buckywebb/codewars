adc5e6fd0f89d89b6c56c40688051b6c85d01a25
buckywebb <bucky_webb@yahoo.com>
2023-06-15 17:48:56 -0400

    Find the Position

---
+++ find-the-position.js
@@ -0,0 +1,11 @@
+/*When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"*/
+
+/*Answer*/
+function position(letter){
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"
+  return `Position of alphabet: ${alphabet.indexOf(letter) + 1}`
+}
