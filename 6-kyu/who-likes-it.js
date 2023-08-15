f499e24067d1b1cc6f0ed4b776bba40655078f5c
buckywebb <bucky_webb@yahoo.com>
2023-08-14 20:51:16 -0400

    Who Likes It?

--- 6-kyu/who-likes-it.js
+++ 6-kyu/who-likes-it.js
@@ -35,3 +35,4 @@ function likes(names) {
       case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
       default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
     }
+}
