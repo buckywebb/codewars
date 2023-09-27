87251d0af7b35cb7d30a660bfd5ca0b7aa5ad199
buckywebb <bucky_webb@yahoo.com>
2023-09-26 23:35:01 -0400

    Name on Billboard

---
+++ 8-kyu/name-on-billboard.js
@@ -0,0 +1,25 @@
+/*You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
+
+You can not use multiplier "*" operator.
+
+If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).*/
+
+/* MY ANSWER */
+function billboard(name, price = 30){
+    let x = name.split('')
+    let cost = 0
+    for (let i = 1; i <= x.length; i++){
+      cost = cost + price
+    }
+    return cost
+  } 
+
+
+/* TOP ANSWER */
+function billboard(name, price = 30){
+    let cost = 0
+    for (let i = 1; i <= x.length; i++){
+      cost += price
+    }
+    return cost
+  } 
