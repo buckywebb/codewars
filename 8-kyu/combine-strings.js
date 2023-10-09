8c9c791daf227bd952a3d2028843d49290fa28a7
buckywebb <bucky_webb@yahoo.com>
2023-10-09 15:05:19 -0400

    Combine Strings Function

---
+++ 8-kyu/combine-strings.js
@@ -0,0 +1,4 @@
+/*Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.*/
+
+/* MY ANSWER */
+const combineNames = (n1,n2) => `${n1} ${n2}`
